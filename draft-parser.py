d71da54b0159ced86635dca4531f0ab5b3c04a5f
mautomic <maurya373@gmail.com>
2020-08-16 16:41:09 -0500

    add team retrieval by draft pick functions

--- draft-parser.py
+++ draft-parser.py
@@ -2,8 +2,9 @@ import csv
 import os
 
 def main():
-	files = []
 
+	# Read in all draft csvs
+	files = []
 	for file in os.listdir("drafts"):
 	    if file.endswith(".csv"):
 	        files.append(os.path.join("drafts", file))
@@ -11,20 +12,44 @@ def main():
 	print("Found " + str(len(files)) + " drafts to analyze")
 
 	drafts = []
-
 	for file in files:
 		picks = []
 		with open(file, 'r') as csvfile:
 			reader = csv.reader(csvfile, delimiter=',')
 			for row in reader:
+				if (row[0] == "Overall Pick"):
+					continue
 				picks.append(row)
-
 		cleansed_picks = clean_data(picks)
-		for clp in cleansed_picks:
-			print(clp)
 		drafts.append(cleansed_picks)
 
 	print("Finished parsing all drafts")
+
+	# Begin any sort of analysis
+	for draft in drafts:
+		print(num_of_positions_taken(draft, "RB", 5))
+		print(num_of_positions_taken(draft, "WR", 5))
+		print(get_team_for_pick_num(draft, get_draft_pick_nums(8)))
+
+def get_draft_pick_nums(pick_num):
+	draft_pick_nums = []
+	for i in range(0, 20):
+		if i % 2 == 0:
+			draft_pick_nums.append((i * 12) + pick_num)
+		elif i % 2 == 1:
+			draft_pick_nums.append((i * 12) + (13 - pick_num))
+	return draft_pick_nums
+
+def get_team_for_pick_num(picks, draft_pick_nums):
+	for i in draft_pick_nums:
+		print(picks[i-1])
+
+def num_of_positions_taken(picks, position, round):
+	counter = 0
+	for i in range(0, round*12):
+		if (picks[i][4] == position):
+			counter = counter + 1
+	return counter
 
 def clean_data(picks):
 	cleansed_picks = []
@@ -34,9 +59,8 @@ def clean_data(picks):
 	 	items.append(pick[1].strip()) # round
 	 	items.append(pick[2].strip()) # pick in round
 	 	player_data = pick[-5:]
-	 	items.append(player_data[1].strip()) # first name
-	 	items.append(player_data[0].strip()) # last name
-	 	items.append(player_data[2].strip()) # positions
+	 	items.append(player_data[1].strip() + " " + player_data[0].strip()) # first + last name
+	 	items.append(player_data[2].strip()) # position
 	 	items.append(player_data[3].strip()) # team
 	 	items.append(player_data[4].strip()) # bye
 	 	cleansed_picks.append(items)

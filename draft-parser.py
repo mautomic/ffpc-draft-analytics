44524dbd16ac04152601fcb57a6a995db4eae601
mautomic <maurya373@gmail.com>
2020-08-16 14:35:24 -0500

    setup for parsing multiple drafts

--- draft-parser.py
+++ draft-parser.py
@@ -1,11 +1,26 @@
 import csv
+import os
 
-read = []
+files = []
 
-with open('drafts/draftresults-0.csv', 'r') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',')
-	for row in reader:
-		read.append(row)
+for file in os.listdir("drafts"):
+    if file.endswith(".csv"):
+        files.append(os.path.join("drafts", file))
 
-for i in read:
-	print(i)
+print("Found " + str(len(files)) + " drafts to analyze")
+
+for file in files:
+
+	print()
+	print("Beginning parsing of " + file)
+	read = []
+
+	with open(file, 'r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
+		for row in reader:
+			read.append(row)
+
+	for i in read:
+		print(i)
+
+print("Finished parsing all drafts")
